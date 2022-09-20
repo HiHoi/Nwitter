13fd5c9eb7a7074cf8f337b43bc370e8351edf53
HiHoi <ghtjd5087@gmail.com>
2022-09-21 00:29:16 +0900

    Add: Img posting & clear

--- src/fbase.js
+++ src/fbase.js
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
 import firebase from "firebase/compat/app";
-import "firebase/compat/auth"
-import "firebase/compat/firestore"
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import "firebase/compat/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,3 +21,4 @@ firebase.initializeApp(firebaseConfig);
 export const firebaseInstance = firebase;
 export const authService = firebase.auth();
 export const dbService = firebase.firestore();
+export const storageService = firebase.storage();
